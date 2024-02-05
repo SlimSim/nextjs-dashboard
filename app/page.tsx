5f5f1f28301fea4d99ca1940f7c5c73c4c8b510f
SlimSim <slimsimapps@gmail.com>
2024-02-05 13:29:05 +0100

    fixing link according to comment :)

--- app/page.tsx
+++ app/page.tsx
@@ -27,6 +27,12 @@ export default function Page() {
             , brought to you by Vercel.
           </p>
           <Link
+            href={'/dashboard'}
+            className="flex items-center gap-5 self-start rounded-lg bg-pink-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          >
+            <span>Dashboard</span> <ArrowRightIcon className="w-5 md:w-6" />
+          </Link>
+          <Link
             href={'/login' as Route}
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
